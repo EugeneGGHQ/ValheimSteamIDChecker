dde381bfec153dbcfd34892bbcf26ac949f12c0e
Eugene <eugene@gghq.io>
2023-03-28 10:21:38 +0100

    Removing individual outputs

--- sidblk.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-#sidblk.sh
-#
-#The below script analyses the contents of the vhserver-console.log - Just update its path as needed in place.
-#
-#-------------
-#Output all data of blacklisted attempts to join the server up to the 8th column, including time stamps, and outputting to sidblk.txt
-#-------------
-grep blacklist vhserver-console.log | cut -f-8 -d' ' | sort -u > sidblk.txt
--- steam64converter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Enter SteamID64 Value to output the friendly 'Name' last registered on SteamID.io
-read -p "Enter SteamID64: " SteamID64
-echo "You entered $SteamID64"
-curl -v silent https://steamid.io/lookup/$SteamID64 2>&1 | grep '"name"'
